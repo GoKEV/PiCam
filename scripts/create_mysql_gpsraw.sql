771484cf4cada17ec13fdecd50b9db46304dbbfd
root <root@kev-tank.kplh>
2018-09-07 21:06:05 -0500

    updated raw GPS for database

---
+++ scripts/create_mysql_gpsraw.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.16  Distrib 10.2.17-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: gpsdata
+-- ------------------------------------------------------
+-- Server version	10.2.17-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `snapshot`
+--
+
+DROP TABLE IF EXISTS `snapshot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `snapshot` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gpstime` varchar(25) NOT NULL,
+  `gpsdate` varchar(25) NOT NULL,
+  `latitude` varchar(25) NOT NULL,
+  `longitude` varchar(25) NOT NULL,
+  `fixqa` int(11) NOT NULL,
+  `satellites` int(11) NOT NULL,
+  `knots` int(11) NOT NULL,
+  `mph` int(11) NOT NULL,
+  `kmph` int(11) NOT NULL,
+  `google_url` varchar(100) NOT NULL,
+  `epoch` int(25) NOT NULL,
+  `nowtime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=47236 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-09-07 21:05:34
